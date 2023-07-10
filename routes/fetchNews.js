09c3778394f4908a6bb3d280e2c480055aa63ae4
Harsh <harsh.jethwani16@gmail.com>
2023-07-10 04:12:30 -0500

    Fix "Cannot set headers after they are sent" error

--- routes/fetchNews.js
+++ routes/fetchNews.js
@@ -7,7 +7,7 @@ router.post("/fetchnews", async (req, res) => {
     // If no body sent with the request
 
     if (Object.keys(req.body).length === 0) {
-        res.status(400).json({
+        return res.status(400).json({
             error: "Request body not found",
             message: "Include all the request parameters in the request body",
         });
@@ -17,7 +17,7 @@ router.post("/fetchnews", async (req, res) => {
 
     // If not all the mandatory parameters sent in the body
     if (!(country && category && APIKey && page && pageSize)) {
-        res.status(400).json({
+        return res.status(400).json({
             error: "Invalid Request Parameters",
             message: "Check the request parameters and try again",
         });
@@ -30,7 +30,7 @@ router.post("/fetchnews", async (req, res) => {
         const data = await fetch(url);
 
         if (data.status !== 200) {
-            res.status(400).json({
+            return res.status(400).json({
                 error: "An unexpected error occured, please try again later",
                 message: "",
             });
@@ -39,7 +39,7 @@ router.post("/fetchnews", async (req, res) => {
         let parsedData = await data.json();
         if (parsedData.status === "error") {
             // The API returns 200 status request even when the API limit is exceeded.
-            res.status(400).json({
+            return res.status(400).json({
                 error: "API limit exceeded, please try again later",
                 message: `Error message:${parsedData.message}`,
             });
@@ -48,7 +48,7 @@ router.post("/fetchnews", async (req, res) => {
         res.json(parsedData);
     } catch (error) {
         console.log(error);
-        res.status(500).json({
+        return res.status(500).json({
             error: "Unexpected Internal Server Error",
             message: "Try again later",
         });
